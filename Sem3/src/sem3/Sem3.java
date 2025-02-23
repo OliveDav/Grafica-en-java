08f842cbbd7cdfcc1791958e95872812471a2f98
OliveDav <136120488+OliveDav@users.noreply.github.com>
2025-02-23 10:50:16 -0600

    Add files via upload

---
+++ Sem3/src/sem3/Sem3.java
@@ -0,0 +1,10 @@
+package sem3;
+
+public class Sem3 {
+
+    public static void main(String[] args) {
+        InterfazGrafic Grafic = new InterfazGrafic();
+        Grafic.setVisible(true);
+    }
+    
+}
